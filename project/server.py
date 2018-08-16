ab072e55eff1e32d19cad9b2e71b42ed1cd2e62a
wangha1 <Hao@HaodeMacBook-Pro.local>
2018-08-15 18:32:58 -0700

    finish page

--- project/server.py
+++ project/server.py
@@ -34,29 +34,7 @@ def search(indexer, searchTerm, searchColumns):
 			counter+=1
 		return result
 
-def search_stats(indexer, searchTerm, searchColumns):
-	with indexer.searcher() as searcher:
-		words = searchColumns
-		query = MultifieldParser(words, schema=indexer.schema).parse(searchTerm)
-		results = searcher.search(query,limit=None)
-		result=[]
-		Header = ['firstName', 'lastName', 'Position', 'Height', 'Weight', 'Class', 'Hometown','Highschool','College']
-		Header += ['CollegeFullName', 'mascot']
-		Header += ["hit_AVG", "hit_GP", "hit_GS", "hit_AB", "hit_R", "hit_H", "hit_2B", "hit_3B", "hit_HR"]
-		Header += ["hit_RBI", "hit_SLG", "hit_BB", "hit_HBP", "hit_SO", "hit_GDP", "hit_OB", "hit_SF"] 
-		Header += ["hit_SH", "hit_SB", "hit_PO", "hit_A", "hit_E", "hit_FLD"]
-		Header += ["pitch_ERA", "pitch_W", "pitch_L", "pitch_APP", "pitch_GS", "pitch_CG", "pitch_SHO"]
-		Header += ["pitch_SV", "pitch_IP", "pitch_H","pitch_R", "pitch_ER", "pitch_BB", "pitch_SO", "pitch_2B"]
-		Header += ["pitch_3B", "pitch_HR", "pitch_BF", "pitch_BAVG", "pitch_WP", "pitch_HBP", "pitch_BK"]
-		Header += ["pitch_SFA", "pitch_SHA"]
-
-		for line in results:
-			for item in Header:
-				result.append([])
-				result[len(result)-1].append(line[item])
-
-		#print(result)
-		return result
+ 
 
 app = Flask(__name__)
 
@@ -93,34 +71,6 @@ def result():
 		img = item['College'] + "/image/" + item["firstName"] + "_" + item["lastName"] + ".jpg"
 		item["img"] = img
 	return render_template('index.html', query=query, results=result,stats=result,scroll="result")
-
-@app.route('/Modal/', methods=['GET', 'POST'])
-def Modal():
-	if request.method == 'POST':
-		data = request.form
-	else:
-		data = request.args
-	query = data.get('query')
-	img = data.get('img')
-	first = data.get('firstName')
-	last = data.get('lastName')
-	coll = data.get('College')
-	keyword = first +' ' + last + ' ' + coll
-	Header = ['firstName', 'lastName', 'Position', 'Height', 'Weight', 'Class', 'Hometown','Highschool','College']
-	Header += ['CollegeFullName', 'mascot']
-	Header += ["hit_AVG", "hit_GP", "hit_GS", "hit_AB", "hit_R", "hit_H", "hit_2B", "hit_3B", "hit_HR"]
-	Header += ["hit_RBI", "hit_SLG", "hit_BB", "hit_HBP", "hit_SO", "hit_GDP", "hit_OB", "hit_SF"] 
-	Header += ["hit_SH", "hit_SB", "hit_PO", "hit_A", "hit_E", "hit_FLD"]
-	Header += ["pitch_ERA", "pitch_W", "pitch_L", "pitch_APP", "pitch_GS", "pitch_CG", "pitch_SHO"]
-	Header += ["pitch_SV", "pitch_IP", "pitch_H","pitch_R", "pitch_ER", "pitch_BB", "pitch_SO", "pitch_2B"]
-	Header += ["pitch_3B", "pitch_HR", "pitch_BF", "pitch_BAVG", "pitch_WP", "pitch_HBP", "pitch_BK"]
-	Header += ["pitch_SFA", "pitch_SHA"]
-	dx = open_dir("indexDir")
-	result = search_stats(dx, keyword, Header)
-	return render_template('detail.html', query=query, img = img,stats=zip(result[0],result[1],result[2],result[3],result[4],result[5],result[6],result[7]), scroll="myModal")
-
-
-
 
 if __name__ == '__main__':
 	app.run(debug=True)
